ea4ffa12acdf4b7cc3c2cc2defb29a46ae6a96b7
totototo0526 <mc.modding.toto@gmail.com>
2025-07-25 18:39:27 +0900

    Add Gradle wrapper and configuration files
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Included gradlew and gradlew.bat scripts for executing Gradle tasks.
    - Created settings.gradle to define the project structure and apply necessary plugins.

---
+++ logicchisel-codegen/app/src/main/java/io/github/totototo0526/logicchisel/codegen/Main.java
@@ -0,0 +1,13 @@
+package io.github.totototo0526.logicchisel.codegen;
+
+public class Main {
+    public static void main(String[] args) {
+        // ここから処理を開始します
+        System.out.println("LogicChisel Code Generator -- Booting up...");
+
+        // ■マイルストーン1の実装タスク
+        // 1. ロギング機構の初期化
+        // 2. config.yml の読み込み処理
+        // 3. DslCodeGenerator のインスタンス化と実行
+    }
+}
